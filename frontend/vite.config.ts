6dc235979dd5414ff1b1244f91272cc6980b6738
Miguel Batista <msb2@cesar.school>
2026-06-07 22:47:29 -0300

    atualizei

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['notebook-wsl']
+  },
   plugins: [
     vue(),
     vueDevTools(),
